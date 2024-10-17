dd608ac9006e3d30e7d1982be70b670095111fd6
Ali <105427281+Alilock@users.noreply.github.com>
2024-10-17 20:10:56 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import HomePage from './pages/HomePage'
+import ProductListPage from './pages/ProductListPage'
+import ProductPage from './pages/ProductPage'
+import BasketPage from './pages/BasketPage'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="productlist" element={<ProductListPage />} />
+        <Route path='product/:id' element={<ProductPage />} />
+        <Route path="basket" element={<BasketPage />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import MenuIcon from './svgs/MenuIcon'
+import ShopIcon from './svgs/ShopIcon'
+const Header = () => {
+    return (
+        <nav className='h-16 flex justify-between border-b-[0.5px] border border-[#121212]' >
+            <div className='flex '>
+                <Link className='px-10 flex items-center text-center justify-center py-6 border sm:border-r-[0.5px] border-t-[0px] border-b-[0.5px] border-[#121212]'>
+                    <p className='max-sm:hidden'>Shop</p>
+                    <div className='sm:hidden'>
+                        <MenuIcon />
+                    </div>
+
+                </Link>
+                <Link className='px-10 max-sm:hidden  flex items-center text-center justify-center py-6 border border-l-[0.5px] border-t-[0px] border-b-[0.5px]  border-[#121212]'>
+                    <p>Contact</p>
+                </Link>
+            </div>
+            <div className='flex'>
+                <Link className='px-10 max-sm:hidden flex items-center text-center justify-center py-6 border border-r-[0.5px] border-t-[0px] border-b-[0.5px] border-[#121212]'>
+                    <p>Sign In</p>
+                </Link>
+                <Link className='px-10 flex items-center text-center justify-center py-6 border sm:border-l-[0.5px] border-t-[0px] border-b-[0.5px] border-[#121212]'>
+                    <p className='max-sm:hidden'>Cart</p>
+                    <div className='sm:hidden'>
+                        <ShopIcon />
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
