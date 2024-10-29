f66c933f761334fdb28077f7047d2d9ffbcb4ae0
Ali <105427281+Alilock@users.noreply.github.com>
2024-10-29 21:04:29 +0400

    Refactor localization files for English and Azerbaijani languages

--- src/App.js
+++ src/App.js
@@ -5,20 +5,46 @@ import HomePage from './pages/HomePage'
 import ProductListPage from './pages/ProductListPage'
 import ProductPage from './pages/ProductPage'
 import BasketPage from './pages/BasketPage'
+import BasketProvider from './context/BasketContext'
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import translationEN from './locales/en.json';
+import translationAZ from './locales/az.json';
 
 
-
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  az: {
+    translation: translationAZ
+  }
+};
+i18n
+  .use(initReactI18next)
+  .init({
+    resources,
+    lng: 'az',
+    fallbackLng: 'en',
+    keySeparator: false,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="productlist/:id" element={<ProductListPage />} />
-        <Route path='product/:id' element={<ProductPage />} />
-        <Route path="basket" element={<BasketPage />} />
-      </Routes>
-    </Router>
+    <BasketProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="productlist/:id" element={<ProductListPage />} />
+          <Route path='product/:id' element={<ProductPage />} />
+          <Route path="basket" element={<BasketPage />} />
+        </Routes>
+      </Router>
+    </BasketProvider>
+
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,43 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import MenuIcon from './svgs/MenuIcon'
 import ShopIcon from './svgs/ShopIcon'
+import { BasketContext } from '../context/BasketContext'
+import { useTranslation } from 'react-i18next'
 const Header = () => {
+    const { basketItems, setBasketItems, handleBasket } = useContext(BasketContext)
+    const { t, i18n } = useTranslation()
     return (
         <nav className='h-16 flex justify-between border-b-[0.5px] border border-[#121212]' >
             <div className='flex w-1/4'>
                 <Link className='w-1/2 flex items-center text-center justify-center border sm:border-r-[0.5px] border-t-[0px] border-b-[0.5px] border-[#121212]'>
-                    <p className='max-lg:hidden'>Shop</p>
+                    <p className='max-lg:hidden'>{t("shop")}</p>
                     <div className='lg:hidden'>
                         <MenuIcon />
                     </div>
 
                 </Link>
                 <Link className='w-1/2 max-lg:hidden  flex items-center text-center justify-center border border-l-[0.5px] border-t-[0px] border-b-[0.5px]  border-[#121212]'>
-                    <p>Contact</p>
+                    <p>{t("contact")}</p>
                 </Link>
+            </div>
+            <div className='w-1/2 flex items-center justify-center'>
+                <div className='flex items-center gap-2'>
+                    <button onClick={() => i18n.changeLanguage("en")} style={{
+                        backgroundColor: i18n.language === "en" ? "red" : "white",
+                    }}>EN</button>
+                    <button style={{
+                        backgroundColor: i18n.language === "az" ? "red" : "white",
+                    }} onClick={() => i18n.changeLanguage("az")}>AZ</button>
+                </div>
             </div>
             <div className='flex w-1/4 justify-end'>
                 <Link className='w-1/2 max-lg:hidden flex items-center text-center justify-center border border-r-[0.5px] border-t-[0px] border-b-[0.5px] border-[#121212]'>
-                    <p>Sign In</p>
+                    <p>{t("signin")}</p>
                 </Link>
                 <Link className='w-1/2 flex items-center text-center justify-center border sm:border-l-[0.5px] border-t-[0px] border-b-[0.5px] border-[#121212]'>
-                    <p className='max-lg:hidden'>Cart</p>
+                    <p className='max-lg:hidden'>{t("cart")} {basketItems.length}</p>
+
                     <div className='lg:hidden'>
                         <ShopIcon />
                     </div>
